9e1630d670de83771cc67498c86fc7adfa3c3b2d
Adnan Hodzic <adnan@hodzic.org>
2019-10-06 15:24:42 +0200

    improved so only allowed input are ints

--- ch01/ExerciseReadIntUntilEnd.go
+++ ch01/ExerciseReadIntUntilEnd.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -14,9 +15,13 @@ func main() {
 	fmt.Println("Enter stuff ...")
 	for scanner.Scan() {
 		fmt.Println(">", scanner.Text())
+		notNumber, err := strconv.Atoi(scanner.Text())
 		if scanner.Text() == "END" {
 			fmt.Println("Reched the END, goodbye")
 			os.Exit(1)
+		} else if err != nil {
+			fmt.Println("Value is not an integer:", notNumber, "\ntry agian ...")
+
 		}
 	}
 }
